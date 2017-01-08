f7999a3c4c9a4a201adf730c09de2fc0e0646585
PierreBtz <PierreBtz@users.noreply.github.com>
2017-01-08 12:35:53 +0100

    Fix indentation

--- src/main/java/pierrebtz/ProjectmRsvpApplication.java
+++ src/main/java/pierrebtz/ProjectmRsvpApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ProjectmRsvpApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProjectmRsvpApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectmRsvpApplication.class, args);
+    }
 }
